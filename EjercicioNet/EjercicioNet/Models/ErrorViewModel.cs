242d7f92dc3ca3dad5825b87009517e74a4382e5
Ubaldo Pérez Valencia <ubaldo_perez_valencia@hotmail.com>
2020-06-23 19:18:41 -0500

    EjercicioNet

---
+++ EjercicioNet/EjercicioNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EjercicioNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
